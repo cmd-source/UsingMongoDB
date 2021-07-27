af725e6ca130868ed2fd7af3734158cd57225f24
cmd-source <conorcrosbie09@gmail.com>
2021-07-27 13:00:36 +0000

    finishing off the connection to Mongo

--- mongo.py
+++ mongo.py
@@ -7,4 +7,19 @@ MONGO_URI = os.environ.get("MONGO_URI")
 DATABASE = "myFirstDB"
 COLLECTION = "celebrities"
 
-def mongo_connect
+def mongo_connect(url):
+    try:
+        conn = pymongo.MongoClient(url)
+        print("Mongo is connented")
+        return conn
+    except pymongo.errors.ConnectionFailure as e:
+        print("Could not connect to MongoDB: %s") % e 
+
+conn = mongo_connect(MONGO_URI)
+
+coll = conn[DATABASE][COLLECTION]
+
+documents = coll.find()
+
+for doc in documents:
+    print(doc)
